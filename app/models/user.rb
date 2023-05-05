007e8fb35886afeedff1e7686e6fc6b8b205f258
MatheusOB21 <matheus53barros@hotmail.com>
2023-05-05 16:04:01 -0400

    Qualquer usuário consegue visitar a home

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :lots
+  #Validaçoes
   validates :cpf, presence: true
   validates :cpf, uniqueness: true
   validates :cpf, length:{ is: 11 }
---
+++ spec/system/home/any_user_acess_home_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'Qualquer usuário consegue' do
+  it 'acessar a pagina inicial' do
+    #Arrange
+
+    #Act
+
+    #Assert
+  end
+end
