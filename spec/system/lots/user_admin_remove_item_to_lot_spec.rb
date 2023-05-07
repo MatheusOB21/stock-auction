17ee2fac06dd344f9e7d2df38686db11b1e44b8c
MatheusOB21 <matheus53barros@hotmail.com>
2023-05-07 08:31:33 -0400

    Usuario só consegue remover items de lotes pendentes

--- spec/system/lots/user_admin_remove_item_to_lot_spec.rb
+++ spec/system/lots/user_admin_remove_item_to_lot_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe 'Usuário admin deleta item' do
+describe 'Usuário admin remove item' do
     it 'com sucesso' do
     #Arrange
       user_admin = User.create!(name: "Flávio", email: "flavio@leilaodogalpao.com.br", password: "flavio_do_leilão", cpf:"50534524079")
@@ -18,5 +18,23 @@ describe 'Usuário admin deleta item' do
       expect(page).to have_content "Item removido com sucesso"    
       expect(page).not_to have_content "Ninja 2000"    
       expect(page).not_to have_content "Uma moto verde, veloz e em ótimo estado"    
+  end
+    it 'apenas de lote com status de pendente' do
+    #Arrange
+      user_admin = User.create!(name: "Flávio", email: "flavio@leilaodogalpao.com.br", password: "flavio_do_leilão", cpf:"50534524079")
+      user_admin_2 = User.create!(name: "Luiz", email: "luiz@leilaodogalpao.com.br", password: "luiz_do_leilão", cpf:"40311733000")
+      
+      lot = Lot.create!(code: "FRA456345", start_date: "28/05/2023", limit_date: "28/06/2023", minimal_val: 50, minimal_difference: 10, user: user_admin)
+      item = Item.create!(name: 'Ninja 2000', description: 'Uma moto verde, veloz e em ótimo estado', weight: 2000, depth: 1000, height: 1500, width: 300, product_category: 'Motocicleta')
+      
+      lot_item = LotItem.create!(lot: lot, item: item)
+    #Act
+      login_as(user_admin_2)
+      visit root_path
+      click_on 'Lotes pendentes'
+      click_on 'FRA456345'
+      click_on 'Aprovar lote'
+    #Assert 
+      expect(page).not_to have_content "Remover item"     
   end
 end
