6d842985d7c0794dcce4a76c07cd963ee1cef45f
MatheusOB21 <matheus53barros@hotmail.com>
2023-05-08 07:51:00 -0400

    Usuario regular não pode se cadastrar com E-mail que já está cadastrado

--- spec/system/authentication/user_regular_authentication_spec.rb
+++ spec/system/authentication/user_regular_authentication_spec.rb
@@ -18,4 +18,21 @@ describe 'Usuário regular ao crair a conta' do
     expect(current_path).not_to eq root_path
     expect(page).to have_content 'Cpf já está em uso'
   end
+  it 'não pode usar o mesmo email de um usuário admin' do
+    #Arrange
+      User.create!(name: "Lívia", email: "admin@leilaodogalpao.com.br", password: "lívia1234", cpf: "14272954091")
+    #Act
+    visit root_path
+    click_on 'Login'
+    click_on 'Criar conta'
+    fill_in 'Nome', with: 'Paulo'
+    fill_in 'CPF', with: '38548644001'
+    fill_in 'E-mail', with: 'admin@leilaodogalpao.com.br'
+    fill_in 'Senha', with: 'admin@1234'
+    fill_in 'Confirme sua senha', with: 'admin@1234'
+    click_on 'Criar conta'
+    #Assert
+    expect(current_path).not_to eq root_path
+    expect(page).to have_content 'E-mail já está em uso'
+  end
 end
