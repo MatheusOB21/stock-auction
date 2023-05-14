2cc46187dd412f2634425790c15ab6e2d1625f27
MatheusOB21 <matheus53barros@hotmail.com>
2023-05-14 10:39:00 -0400

    Teste para garantir associação de lot_item

--- spec/models/lot_item_spec.rb
+++ spec/models/lot_item_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe LotItem, type: :model do
+  describe 'tem referencia a' do
+    it 'lote' do
+      #Arrange
 
+      #Act
+        result = LotItem.reflect_on_association(:lot).macro
+      #Assert
+        expect(result).to eq :belongs_to
+    end
+    it 'usuário' do
+      #Arrange
+
+      #Act
+        result = LotItem.reflect_on_association(:item).macro
+      #Assert
+        expect(result).to eq :belongs_to
+    end
+  end
 end
