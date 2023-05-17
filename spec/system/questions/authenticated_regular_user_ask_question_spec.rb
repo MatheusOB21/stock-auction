e665f7a8f3fe278d358af656829feb60ecbaaa7d
MatheusOB21 <matheus53barros@hotmail.com>
2023-05-17 08:06:12 -0400

    Usuário regular vê pergunta sem resposta, no detalhes do lote

--- spec/system/questions/authenticated_regular_user_ask_question_spec.rb
+++ spec/system/questions/authenticated_regular_user_ask_question_spec.rb
@@ -65,27 +65,27 @@ describe 'Usuario regular faz uma pergunta' do
   end
 
   it 'com sucesso, e vê a pergunta no lote' do
-  #Arrange
-    user_admin = User.create!(name: "Flávio", email: "flavio@leilaodogalpao.com.br", password: "flavio_do_leilão", cpf:"50534524079")
-    user_regular = User.create!(name: "Katarina", email: "katarina@gmail.com.br", password: "katarina12345", cpf:"09036567017")
-      
-    item = Item.create!(name: 'Ninja 2000', description: 'Uma moto verde, veloz e em ótimo estado', weight: 2000, depth: 1000, height: 1500, width: 300, product_category: 'Motocicleta')
+    #Arrange
+      user_admin = User.create!(name: "Flávio", email: "flavio@leilaodogalpao.com.br", password: "flavio_do_leilão", cpf:"50534524079")
+      user_regular = User.create!(name: "Katarina", email: "katarina@gmail.com.br", password: "katarina12345", cpf:"09036567017")
+        
+      item = Item.create!(name: 'Ninja 2000', description: 'Uma moto verde, veloz e em ótimo estado', weight: 2000, depth: 1000, height: 1500, width: 300, product_category: 'Motocicleta')
 
-    lot = Lot.create!(code: "RTX306000", start_date: Date.today, limit_date: 15.day.from_now, minimal_val: 200, minimal_difference: 50, user: user_admin, status: 'aprovated')
-    lot_item = LotItem.create!(lot: lot, item: item)
-  
-  #Act
-    login_as(user_regular)
-    visit root_path
-    click_on 'RTX306000'
-    click_on 'Faça uma pergunta sobre o lote!'
-    fill_in 'Pergunta', with: 'Qual o estado dos itens presente no lote?'
-    click_on 'Enviar'
-  #Assert
-    expect(current_path).to eq lot_path(lot.id)
-    expect(page).to have_content 'Perguntas sobre o lote'    
-    expect(page).to have_content 'Dúvidas dos usuário:'    
-    expect(page).to have_content 'Qual o estado dos itens presente no lote?'    
+      lot = Lot.create!(code: "RTX306000", start_date: Date.today, limit_date: 15.day.from_now, minimal_val: 200, minimal_difference: 50, user: user_admin, status: 'aprovated')
+      lot_item = LotItem.create!(lot: lot, item: item)
+    
+    #Act
+      login_as(user_regular)
+      visit root_path
+      click_on 'RTX306000'
+      click_on 'Faça uma pergunta sobre o lote!'
+      fill_in 'Pergunta', with: 'Qual o estado dos itens presente no lote?'
+      click_on 'Enviar'
+    #Assert
+      expect(current_path).to eq lot_path(lot.id)
+      expect(page).to have_content 'Perguntas sobre o lote'    
+      expect(page).to have_content 'Dúvidas dos usuário:'    
+      expect(page).to have_content 'Qual o estado dos itens presente no lote?'    
   end
   
   it 'sem sucesso, pois deixou  o campo em branco' do
---
+++ spec/system/questions/regular_user_view_question_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Usuário regular vê pergunta' do
+  it 'sem resposta, no detalhes do lote' do
+  #Arrange
+    user_admin = User.create!(name: "João", email: "joao@leilaodogalpao.com.br", password: "joao_do_leilão", cpf:"50534524079")
+    user_regular = User.create!(name: "Katara", email: "katara@gmail.com.br", password: "katara12345", cpf:"09036567017")
+      
+    item = Item.create!(name: 'Ninja 2000', description: 'Uma moto verde, veloz e em ótimo estado', weight: 2000, depth: 1000, height: 1500, width: 300, product_category: 'Motocicleta')
+
+    lot = Lot.create!(code: "RTX306000", start_date: Date.today, limit_date: 15.day.from_now, minimal_val: 200, minimal_difference: 50, user: user_admin, status: 'aprovated')
+    lot_item = LotItem.create!(lot: lot, item: item)
+
+    Question.create!(lot: lot, question: "Os itens estão novos?")
+  
+  #Act
+    login_as(user_regular)
+    visit root_path
+    click_on 'RTX306000'
+  #Assert
+    expect(page).to have_content 'Os itens estão novos?'
+    expect(page).to have_content 'Ainda não respondida'  
+  end
+end
