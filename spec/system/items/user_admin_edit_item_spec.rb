d90a624ddf9f72234f1f4c62cc29b89eaf311f2d
MatheusOB21 <matheus53barros@hotmail.com>
2023-05-18 10:22:32 -0400

    Usuário admin edita item sem sucesso, pois deixa campo em branco

--- spec/system/items/user_admin_edit_item_spec.rb
+++ spec/system/items/user_admin_edit_item_spec.rb
@@ -80,5 +80,30 @@ describe 'Usuário admin edita item' do
       expect(page).to have_content '4000'
       expect(page).to have_content 'Motocicleta' 
   end
+  
+  it 'sem sucesso, pois deixa campo em branco' do
+    #Arrange
+      user_admin = User.create!(name: 'Maria', email: 'maria@leilaodogalpao.com.br', password: 'maria1234', cpf:'94225136000')
+      item = Item.create!(name: 'Yamaha 200', description: 'Uma moto verde, veloz e em ótimo estado', weight: 2000, depth: 1000, height: 1500, width: 300, product_category: 'Motocicleta')
+
+    #Act
+      login_as(user_admin)
+      visit root_path
+      click_on 'Itens disponíveis'
+      within("#1") do
+        click_on 'Editar'
+      end
+      fill_in 'Nome', with: 'Yamaha 200'
+      fill_in 'Descrição', with:''
+      fill_in 'Peso', with: '3000'
+      fill_in 'Profundidade', with: ''
+      fill_in 'Altura', with: '4000'
+      fill_in 'Categoria do Produto', with: 'Motocicleta' 
+      click_on 'Enviar'
+    #Assert
+      expect(page).to have_content 'Item não pode ser editado'
+      expect(page).to have_content 'Descrição não pode ficar em branco'
+      expect(page).to have_content 'Profundidade não pode ficar em branco'
+  end
 
 end
