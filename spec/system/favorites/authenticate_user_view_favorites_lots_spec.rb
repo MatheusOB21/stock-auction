7f7e0376019af49c54045b8907905aa64a149532
MatheusOB21 <matheus53barros@hotmail.com>
2023-05-18 17:41:52 -0400

    Usuário autenticado vê os lotes favoritos e apenas se estiver autenticado

--- spec/system/favorites/authenticate_user_view_favorites_lots_spec.rb
+++ spec/system/favorites/authenticate_user_view_favorites_lots_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 describe 'Usuário autenticado vê os lotes favoritos' do
+
+  it 'e apenas se estiver autenticado' do
+    #Arrange
+      user_admin = User.create!(name: "Flávio", email: "flavio@leilaodogalpao.com.br", password: "flavio_do_leilão", cpf:"50534524079")
+
+      item = Item.create!(name: 'Ninja 2000', description: 'Uma moto verde, veloz e em ótimo estado', weight: 2000, depth: 1000, height: 1500, width: 300, product_category: 'Motocicleta')
+      lot = Lot.create!(code: "ZTZ456789", start_date: Date.today, limit_date: 10.day.from_now, minimal_val: 50, minimal_difference: 10, user: user_admin, status: 'aprovated')
+      lot_item = LotItem.create!(lot: lot, item: item)
+    #Act
+      visit favorites_path
+    #Assert
+      expect(current_path).to eq new_user_session_path     
+  end
   
   it 'a partir do link do menu' do
     #Arrange
