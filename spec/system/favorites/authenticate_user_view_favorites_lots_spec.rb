8beeb06802202c71926524b910112e0e2c20584f
MatheusOB21 <matheus53barros@hotmail.com>
2023-05-18 14:11:14 -0400

    Usuário autenticado vê os lotes favoritos a partir do link do menu

---
+++ spec/system/favorites/authenticate_user_view_favorites_lots_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Usuário autenticado vê os lotes favoritos' do
+  it 'a partir do link do menu' do
+  #Arrange
+    user_admin = User.create!(name: "Flávio", email: "flavio@leilaodogalpao.com.br", password: "flavio_do_leilão", cpf:"50534524079")
+    user = User.create!(name: 'Luke', email: 'luke@gmail.com', cpf:'08522429057', password: 'luke@12345')
+
+    item = Item.create!(name: 'Ninja 2000', description: 'Uma moto verde, veloz e em ótimo estado', weight: 2000, depth: 1000, height: 1500, width: 300, product_category: 'Motocicleta')
+    lot = Lot.create!(code: "ZTZ456789", start_date: Date.today, limit_date: 10.day.from_now, minimal_val: 50, minimal_difference: 10, user: user_admin, status: 'aprovated')
+    lot_item = LotItem.create!(lot: lot, item: item)
+  #Act
+    login_as(user)
+    visit root_path
+  #Assert
+    expect(page).to have_link 'Lotes favoritos'    
+  end
+end
