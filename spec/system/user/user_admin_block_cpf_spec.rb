f6ed21d8a959d803944684336311d89910f2744e
MatheusOB21 <matheus53barros@hotmail.com>
2023-05-24 15:15:47 -0400

    Usuário admin bloqueia um CPF sem sucesso, pois informa um CPF em branco

--- spec/system/user/user_admin_block_cpf_spec.rb
+++ spec/system/user/user_admin_block_cpf_spec.rb
@@ -27,4 +27,19 @@ describe 'Usuário admin bloqueia um CPF' do
 
     expect(Blacklist.last.cpf).to eq '90347825060'
   end
+
+  it  'sem sucesso, pois informa um CPF em branco' do
+    user_admin = User.create!(name: "Paulo", email: "paulo@leilaodogalpao.com.br", cpf: '41505837065', password: 'paulo123456789')
+    user = User.create!(name: "Laura", email: "laura@gmail.com", cpf: '90347825060', password: 'laura123456789')
+
+    login_as(user_admin)
+    visit root_path
+    click_on 'Usuários regulares'
+    click_on 'Bloqueio de CPF'
+    fill_in 'CPF', with: '50545678910'
+    click_on 'Enviar'
+
+    expect(page).to have_content 'CPF é inválido'
+  end
+
 end
